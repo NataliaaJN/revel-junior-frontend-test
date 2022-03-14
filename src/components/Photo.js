f4bf0930426fe6b046aadecb548955657b964037
Natalia <atvnataliajn@gmail.com>
2022-03-14 18:40:37 +0100

    add hover functionality

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import "../styles/components/Photo.scss";
 
 const Photo = ({ eachPhoto, index }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  const renderAuthor = () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (screen.width > 1024) return <p>{eachPhoto.author}</p>;
+  };
+
   const getIndexNumber = () => {
     if (index < 9) {
       return (
@@ -18,8 +26,9 @@ const Photo = ({ eachPhoto, index }) => {
     }
   };
   return (
-    <li className="photoElement" alt="display-photo">
-      <div className="photoElement__photoContainer">
+    <li className="photoElement">
+      <div className="photoElement__photoContainer" onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>
         <div className="photoElement__photoContainer--indexContainer">
           {getIndexNumber()}
         </div>
@@ -29,6 +38,8 @@ const Photo = ({ eachPhoto, index }) => {
           alt={eachPhoto}
         />
       </div>
+      
+      {isShown && <div> {renderAuthor()}</div>}
     </li>
   );
 };
